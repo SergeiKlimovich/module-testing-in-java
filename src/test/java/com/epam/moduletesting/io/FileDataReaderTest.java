0a38d56c65bcc53335d33a63643c249b39d2e065
Sergei Klimovich <Sergei_Klimovich@epam.com>
2022-12-12 22:49:00 +0300

    added FileDataPublisherTest, FileDataReaderTest

--- src/test/java/com/epam/moduletesting/io/FileDataReaderTest.java
+++ src/test/java/com/epam/moduletesting/io/FileDataReaderTest.java
@@ -1,4 +1,37 @@
 package com.epam.moduletesting.io;
 
-public class FileDataReaderTest {
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import com.epam.moduletesting.exception.WrongPathException;
+
+@Tag("io")
+class FileDataReaderTest {
+
+    private final FileDataReader sut = new FileDataReader();
+
+    @Test
+    @EnabledOnOs(value = OS.WINDOWS)
+    void shouldReadDataFromFile() {
+        String filePath = "src/test/resources/test.txt";
+        String expected = "Test data\nTest data1\nTest data2";
+
+        String actual = sut.readData(filePath);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    @EnabledOnOs(value = OS.WINDOWS)
+    void shouldThrowRuntimeExceptionWhenNonExistingFileGiven() {
+        String nonExistingFilePath = "src/test/resources/invalidPath.txt";
+
+        assertThrows(WrongPathException.class, () -> sut.readData(nonExistingFilePath));
+    }
 }
